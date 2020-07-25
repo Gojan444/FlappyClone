df77bbc2d5f05e8b528a41933cbdb514b6a252a3
Johan Andres Acosta Ortiz <gojan4.0@gmail.com>
2020-07-24 22:56:48 -0500

    añadiendo script

---
+++ FlappyClone/Assets/Scripts/flyBird.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyBird : MonoBehaviour
+{
+
+  public float velocity = 1;
+  private Rigibody2D rigibody;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    rb = GetComponent<Rigibody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.)
+
+  }
+
+
+}
